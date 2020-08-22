7630099efd74247b9c7cb0dc509466c07cc1205f
b3z9a <jennabains@gmail.com>
2020-08-21 20:39:10 -0700

    Updated Search Bar Buttons and ReadMe

--- app/README.md
+++ app/README.md
@@ -3,8 +3,9 @@ The frontend part was created using react
 The main component is in **App.js**
 Proxy was configured for url `localhost:8080`
 ### Main logic
-1. Table that displays all addresses
-2. Create and edit addresses
+1. Table that displays all contacts in the address book
+2. User can: 
+**add**, **delete**, **edit**, and **search** contacts in the address book.
 **Cross Origins are not configured**
 ## How to Test
 1. Open the project and run the following commands:
@@ -14,4 +15,4 @@ cd app
 yarn start
 ```
 ## Picture of Homepage
-![alt tag](https://user-images.githubusercontent.com/41052053/90944013-5ad80d00-e3d1-11ea-8e1e-61f69e98c9ee.png)
+![alt tag](https://user-images.githubusercontent.com/41052053/90947838-e8c1f100-e3ed-11ea-8be7-23410787b274.png)
--- app/src/AddressList.js
+++ app/src/AddressList.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button, ButtonGroup, Table} from 'reactstrap';
-import Link from "react-router-dom";
-import AddressEdit from "./AddressEdit";
+import {Button, ButtonGroup, Input, Table} from 'reactstrap';
 
 class AddressList extends Component {
     state = {
         isLoading: true,
         addresses: [],
+        address_search: undefined,
+        firstName_search: undefined,
+        lastName_search: undefined
     };
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
 
     async componentDidMount() {
         const response = await fetch('/addresses');
         const body = await response.json();
         console.log(body);
         this.setState({addresses: body, isLoading: false});
+    }
+
+    handleChange(event) {
+        console.log("HERE");
+        this.setState({firstName_search: event.target.value});
+        console.log(this.state.firstName_search);
+        this.setState({lastName_search: event.target.value});
+        console.log(this.state.lastName_search);
+        this.setState({address_search: event.target.value});
+        console.log(this.state.address_search);
     }
 
     render() {
@@ -32,9 +50,21 @@ class AddressList extends Component {
                     <div className="App-intro">
                         <h2>Address Book</h2>
                     </div>
-{/*                    <div className="float-right">
-                        <input type="text" className="input" placeholder="Search..."/>
-                    </div>*/}
+                    <div>
+                        <Input style={{width:"50%", display:"initial"}} name="search_firstName" id="search_firstName"
+                               onChange={this.handleChange}/>
+                        <button onClick={() => this.search_firstName()}>Search by First Name</button>
+                    </div>
+                    <div>
+                        <Input style={{width:"50%", display:"initial"}} name="search_firstName" id="search_lastName"
+                               onChange={this.handleChange}/>
+                        <button onClick={() => this.search_firstName()}>Search by Last Name</button>
+                    </div>
+                    <div>
+                        <Input style={{width:"50%", display:"initial"}} name="search_address" id="search_address"
+                               onChange={this.handleChange}/>
+                        <button onClick={() => this.search_address()}>Search by Address</button>
+                    </div>
                     <Table className="mt-4">
                         <thead>
                         <tr>
@@ -50,7 +80,8 @@ class AddressList extends Component {
                                 <td> {address.address}</td>
                                 <td>
                                     <ButtonGroup>
-                                        <Button size="sm" color="primary" onClick={()=>this.edit('/edit/'+address.id)}>Edit</Button>
+                                        <Button size="sm" color="primary"
+                                                onClick={() => this.edit('/edit/' + address.id)}>Edit</Button>
                                         <Button size="sm" color="danger"
                                                 onClick={() => this.remove(address.id)}>Delete</Button>
                                     </ButtonGroup>
@@ -62,7 +93,7 @@ class AddressList extends Component {
                         </tbody>
                     </Table>
                     <div className="float-margin-bottom">
-                        <Button color="success" onClick={()=>this.edit('/edit/new')}>Add Contact</Button>
+                        <Button color="success" onClick={() => this.edit('/edit/new')}>Add Contact</Button>
                     </div>
                 </header>
             </div>
@@ -86,6 +117,68 @@ class AddressList extends Component {
 
     edit(path) {
         this.props.history.push(path);
+    }
+
+    // search by address
+    async search_address() {
+        if(this.state.addresses === undefined || this.state.addresses.length === 0){
+            const response = await fetch('/addresses');
+            const body = await response.json();
+            this.setState({addresses: body});
+        } else{
+            const response = await fetch('/addresses'+'?address='+this.state.address_search);
+            const status = await response.status;
+            console.log(status);
+            if(status !== 204){
+                const body = await response.json();
+                console.log(body);
+                this.setState({addresses:body});
+            } else{
+                this.setState({addresses:[]});
+            }
+
+        }
+    }
+
+    // search by first name
+    async search_firstName(){
+        if(this.state.addresses === undefined || this.state.addresses.length === 0){
+            const response = await fetch('/addresses');
+            const body = await response.json();
+            this.setState({addresses: body});
+        } else{
+            const response = await fetch('/addresses'+'?firstName='+this.state.firstName_search);
+            const status = await response.status;
+            console.log(status);
+            if(status !== 204){
+                const body = await response.json();
+                console.log(body);
+                this.setState({addresses:body});
+            } else{
+                this.setState({addresses:[]});
+            }
+        }
+    }
+
+
+    // search by last name
+    async search_lastName(){
+        if(this.state.addresses === undefined || this.state.addresses.length === 0){
+            const response = await fetch('/addresses');
+            const body = await response.json();
+            this.setState({addresses: body});
+        } else{
+            const response = await fetch('/addresses'+'?lastName='+this.state.lastName_search);
+            const status = await response.status;
+            console.log(status);
+            if(status !== 204){
+                const body = await response.json();
+                console.log(body);
+                this.setState({addresses:body});
+            } else{
+                this.setState({addresses:[]});
+            }
+        }
     }
 }
 
